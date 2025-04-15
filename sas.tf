93e1c693343983feb03caec5c910d3a171af9820
itariq20 <31933916+itariq20@users.noreply.github.com>
2025-04-14 18:30:32 -0700

    Create sas.tf

---
+++ sas.tf
@@ -0,0 +1,36 @@
+data "aws_vpc" "this" {
+  filter {
+    name   = "tag:Name"
+    values = ["prod-shared-vpc"]
+  }
+
+}
+
+
+data "aws_vpc" "cmh" {
+  provider = aws.cmh
+  filter {
+    name   = "tag:Name"
+    values = ["prod-vpc"]
+  }
+
+}
+
+
+resource "aws_route53_zone" "this" {
+  name    = "*********"
+  comment = "prod-shared services"
+  vpc {
+    vpc_id = data.aws_vpc.this.id
+  }
+
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+}
+
+resource "aws_route53_zone_association" "secondary" {
+  provider = aws.cmh
+  zone_id  = aws_route53_zone.this.zone_id
+  vpc_id   = data.aws_vpc.cmh.id
+}
