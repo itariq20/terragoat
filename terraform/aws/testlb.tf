bee965a7ef2f5551c7f5b66702782c7076707778
itariq20 <31933916+itariq20@users.noreply.github.com>
2025-10-20 18:57:43 -0700

    Create testlb.tf

---
+++ terraform/aws/testlb.tf
@@ -0,0 +1,42 @@
+# Create a new load balancer
+resource "aws_elb" "weblb" {
+  name = "weblb-terraform-elb"
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  subnets                     = [aws_subnet.web_subnet.id]
+  security_groups             = [aws_security_group.web-node.id]
+  instances                   = [aws_instance.web_host.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = merge({
+    Name = "foobar-terraform-elb"
+    }, {
+    git_commit           = "5c6b5d60a8aa63a5d37e60f15185d13a967f0542"
+    git_file             = "terraform/aws/elb.tf"
+    git_last_modified_at = "2021-05-02 10:06:10"
+    git_last_modified_by = "nimrodkor@users.noreply.github.com"
+    git_modifiers        = "nimrodkor/orshapira"
+    git_org              = "itariq20"
+    git_repo             = "terragoat"
+    yor_trace            = "b4a83ce9-9a45-43b4-b6d9-1783c282f702"
+    }, {
+    yor_name = "weblb"
+  })
+}
